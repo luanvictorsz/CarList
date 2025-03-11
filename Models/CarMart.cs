b6302f4e51c99295de9a5615b6cc8787d7d68e94
victor <luanvitorlima2017@gmail.com>
2025-03-11 17:32:32 -0300

    iniciando estrutura do projeto .net maui, carlist

---
+++ Models/CarMart.cs
@@ -0,0 +1,8 @@
+﻿namespace CarList.Models
+{
+    public class CarMart
+    {
+        public int Dd { get; set; }
+        public List<Car> Cars { get; set; }
+    }
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace CarList;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
